dd4aae54b9fdb995281ec9c3b30462cfb5c73a0b
Ayushi Kumari <115365818+ayushik10@users.noreply.github.com>
2024-01-09 19:14:43 +0530

    Add files via upload

---
+++ Python Course -- CODING BLOCKS/FirstProgram.py
@@ -0,0 +1,9 @@
+print("Multiline Comment")
+'''
+this is a multiline comment
+'''
+print("Comment")
+#this is a single line comment
+print("Multiple Line Comment")
+"""this is a 
+multiple line comment"""
