b3ece9e3ca663bf50e39ecf564f16394b5aa110c
ayushik10 <ayushik1603@gmail.com>
2024-01-10 21:58:38 +0530

    Changes to be committed: 	modified:   Python Course -- CODING BLOCKS/KeywordArguments.py

--- Python Course -- CODING BLOCKS/KeywordArguments.py
+++ Python Course -- CODING BLOCKS/KeywordArguments.py
@@ -5,4 +5,25 @@ def show(a, b, c):
     print(b)
     print(c)
 
-show("Hello", "Hii", "ByeBye")
+# show("Hello", "Hii", "ByeBye")
+# show(b="Hello", c="Hii", a="ByeBye")
+
+def display(a, b, c, d="Python", e="course"):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+
+# display("hello", "world", "python", e="ayushi")
+
+# print("ayushi", "kumari", "is", "a", "BCA", "Student")
+    
+def show(a, b, c, *args, d=10, **kwargs):
+    print(a)
+    print(args)
+    print(d)
+    print(c)
+    print(kwargs)
+
+show(1,2,3, "python", d=100, name="ayushi")
