6748f5f6df0000cb7124d19d62a722dd6b66f606
ayushik10 <ayushik1603@gmail.com>
2024-01-10 09:11:46 +0530

    Changes to be committed: 	modified:   Python Course -- CODING BLOCKS/Operators.py

--- Python Course -- CODING BLOCKS/Operators.py
+++ Python Course -- CODING BLOCKS/Operators.py
@@ -9,3 +9,15 @@ print(a**b)
 a,b,c=10,20,30
 
 print(a,b,c)
+
+print("abc">"ABC")
+
+
+print(isinstance(True, int))
+print(5+True)
+print(type(True))
+print(not True)
+print(not False)
+print(True and False)
+print(True or False)
+print(5 and 0)
